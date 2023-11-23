00453887ac604d1f8f3f9d1243f5448ecff1d605
bakNa2t <ok2.lock@gmail.com>
2023-11-23 10:43:25 +0300

    Add header component to app, update styles

---
+++ src/components/UI/Header.js
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>ContactKeeper</h1>
+    </div>
+  );
+};
+
+export default Header;

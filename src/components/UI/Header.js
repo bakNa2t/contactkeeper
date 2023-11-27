f2228d452637e2c0026d6079877fe5cde90773fe
bakNa2t <ok2.lock@gmail.com>
2023-11-27 17:42:25 +0300

    Add header' component contact icon

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,9 +1,13 @@
 import styles from "./Header.module.css";
+import { IoIosContacts } from "react-icons/io";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <h1>ContactKeeper</h1>
+      <h1>
+        ContactKeeper
+        <IoIosContacts />
+      </h1>
     </div>
   );
 };
